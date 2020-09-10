6d2c3b1c774f5bbecb456fb8216436e1993efe6d
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-09-10 11:55:54 +0200

    Update arrays.rb

--- Intro to Programming/Arrays/arrays.rb
+++ Intro to Programming/Arrays/arrays.rb
@@ -65,3 +65,79 @@ end
 print even_nums(10) # => [0, 2, 4, 6, 8, 10]
 puts
 print even_nums(5)  # => [0, 2, 4]
+
+
+15.Range
+#Write a method range(min, max) that takes in two numbers min and max. 
+#The function should return an array containing all numbers from min to max inclusive.
+
+
+def range(min, max)
+  array = []
+  i = 0
+  
+  while i <= max - min  
+
+    array << min+i
+    
+    i+=1
+    
+  end
+  return array 
+end
+
+print range(2, 7)   # => [2, 3, 4, 5, 6, 7]
+puts
+print range(13, 20) # => [13, 14, 15, 16, 17, 18, 19, 20]
+
+
+16.Odd Range
+#Write a method odd_range(min, max) that takes in two numbers min and max. 
+#The method should return an array containing all odd numbers from min to max (inclusive).
+
+
+def odd_range(min, max)
+
+   array = []
+  i = 0
+  while min <= max   
+    
+if min % 2 != 0
+    array << min
+    
+end   
+    min+=1
+    i+=1
+  end
+  return array 
+end
+
+print odd_range(11, 18) # => [11, 13, 15, 17]
+puts
+print odd_range(3, 7)   # => [3, 5, 7]
+
+
+
+17.Reverse Range
+#Write a method reverse_range(min, max) that takes in two numbers min and max. 
+#The function should return an array containing all numbers from min to max in reverse order. 
+#The min and max should be excluded from the array.
+
+
+def reverse_range(min, max)
+   reversed_array = []
+  
+  i = max - 1
+  while i > min
+     reversed_array << i
+    
+    i -= 1
+  end
+  
+return reversed_array
+end
+
+print reverse_range(10, 17) # => [16, 15, 14, 13, 12, 11]
+puts
+print reverse_range(1, 7)   # => [6, 5, 4, 3, 2]
+
