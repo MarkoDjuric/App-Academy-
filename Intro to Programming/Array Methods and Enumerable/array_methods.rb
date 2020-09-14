15e18c15861b085ced97b86726c2f6a03d0fe170
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-09-14 11:43:13 +0200

    Update array_methods.rb

--- Intro to Programming/Array Methods and Enumerable/array_methods.rb
+++ Intro to Programming/Array Methods and Enumerable/array_methods.rb
@@ -156,3 +156,74 @@ puts is_valid_email("jdoe42@gmail.com") # => false
 puts is_valid_email("jdoegmail.com")    # => false
 puts is_valid_email("az@email")         # => false
 
+
+
+29.Reverse Words
+#Write a method reverse_words that takes in a sentence string and returns the sentence 
+#with the order of the characters in each word reversed. 
+#Note that we need to reverse the order of characters in the words, do not reverse the order of words in the sentence.
+
+
+def reverse_words(str)
+  reversedString = ''
+
+   i=1
+  
+   while i <= str.length
+       reversedString += str[str.length-i]
+  
+      i+=1
+   end
+  
+  arrNew = reversedString.split(' ')
+  
+  reversedString2 = []
+  
+    z = 1
+  
+    while z <= arrNew.lengt
+      reversedString2 << arrNew[arrNew.length-z
+        
+      z+=1
+    end
+
+  return reversedString2.join(' ')
+end
+
+puts reverse_words('keep coding') # => 'peek gnidoc'
+puts reverse_words('simplicity is prerequisite for reliability') # => 'yticilpmis si etisiuqererp rof ytilibailer'
+
+
+  
+29.Reverse Words (SECOND SOLUTION)
+#Write a method reverse_words that takes in a sentence string and returns the sentence 
+#with the order of the characters in each word reversed. 
+#Note that we need to reverse the order of characters in the words, do not reverse the order of words in the sentence.  
+
+
+def reverse_words(sent)
+     array =  sent.split(" ")
+     string = []
+  
+  
+     array.each_with_index  do |word, i|
+       reversed =  reversed_arr(word)
+       string << reversed
+   
+     end
+  
+  return p string.join(" ")
+end
+
+  def reversed_arr(word)
+    str=""
+    
+     word.each_char do |el|
+      str = el + str
+     end
+    
+    return str
+  end
+
+puts reverse_words('keep coding') # => 'peek gnidoc'
+puts reverse_words('simplicity is prerequisite for reliability') # => 'yticilpmis si etisiuqererp rof ytilibailer'
