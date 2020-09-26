f87179d63a10b9bc983b8bf5611849cd545d6d4b
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-09-26 09:47:59 +0200

    Update array_methods.rb

--- Intro to Programming/Array Methods and Enumerable/array_methods.rb
+++ Intro to Programming/Array Methods and Enumerable/array_methods.rb
@@ -255,6 +255,38 @@ puts
 print rotate_array([ "a", "b", "c", "d" ], 2) # => [ "c", "d", "a", "b" ]
 puts  
   
+    
+  
+7.Pig Latin Word
+#Write a method pig_latin_word that takes in a word string and translates the word into pig latin.  
+   
+# Pig latin translation uses the following rules:
+# - for words that start with a vowel, add 'yay' to the end
+# - for words that start with a nonvowel, move all letters before the first vowel to the end of the word and add 'ay'
+
+def pig_latin_word(word)
+  a = word[0]
+  str=""
+  
+vowels = 'aeiou'
+
+  if vowels.include?(a)
+    return word + 'yay'
+  else
+    word.each_char.with_index  do |el,i|
+      if vowels.include?(el)
+      return  word[i..-1] + word[0...i] + 'ay'
+      end
+    end
+  end
+
+end
+
+puts pig_latin_word("apple")   # => "appleyay"
+puts pig_latin_word("eat")     # => "eatyay"
+puts pig_latin_word("banana")  # => "ananabay"
+puts pig_latin_word("trash")   # => "ashtray"  
+  
   
   
   
