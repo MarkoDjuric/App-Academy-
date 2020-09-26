e50d54a8f8748d3d184e08497963da1ac5287534
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-09-26 09:48:44 +0200

    Update array_methods.rb

--- Intro to Programming/Array Methods and Enumerable/array_methods.rb
+++ Intro to Programming/Array Methods and Enumerable/array_methods.rb
@@ -289,4 +289,30 @@ puts pig_latin_word("trash")   # => "ashtray"
   
   
   
+33.Combinations
+#Write a method combinations that takes in an array of unique elements, 
+#the method should return a 2D array representing all possible combinations of 2 elements of the array.  
+  
+
+  def combinations(arr)
+  array = []
+  
+  arr.each_with_index  do |el, i|
+    arr.each_with_index do |char, idx|
+      if idx > i
+      array << [el , char ]
+      end
+    end
+  end
+  return array
+end
+
+print combinations(["a", "b", "c"]); # => [ [ "a", "b" ], [ "a", "c" ], [ "b", "c" ] ]
+puts
+
+print combinations([0, 1, 2, 3]); # => [ [ 0, 1 ], [ 0, 2 ], [ 0, 3 ], [ 1, 2 ], [ 1, 3 ], [ 2, 3 ] ]
+puts  
+  
+  
+  
   
