dd592262c0011be1e70a5321222f632f6978e051
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-09-26 10:01:40 +0200

    Update array_methods.rb

--- Intro to Programming/Array Methods and Enumerable/array_methods.rb
+++ Intro to Programming/Array Methods and Enumerable/array_methods.rb
@@ -362,6 +362,31 @@ end
 puts two_d_sum([ [4, 5],[1, 3, 7, 1]])    # => 21
 puts two_d_sum([[3, 3],[2],[2, 5]])   # => 15
 
+
+11.Two D Translate
+#Write a method two_d_translate that takes in a 2 dimensional array and translates it into a 1 dimensional array. 
+#You can assume that the inner arrays always have 2 elements. See the examples.
+
+
+def two_d_translate(arr)
+    str = []
+
+   arr.each do |el| 
+     el[-1].times do
+        str << el[0]
+     end
+     
+  end
+  
+ return str
+end
+  
+ 
+print two_d_translate([['boot', 3],['camp', 2],['program', 0]]) # => [ 'boot', 'boot', 'boot', 'camp', 'camp' ]
+puts
+print two_d_translate([['red', 1],['blue', 4]]) # => [ 'red', 'blue', 'blue', 'blue', 'blue' ]
+puts
+
   
   
   
