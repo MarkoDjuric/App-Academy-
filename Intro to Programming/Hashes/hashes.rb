2377d0c80ab1b940e9894c766b3b0b7b5e7a318e
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-09-28 23:16:20 +0200

    Update hashes.rb

--- Intro to Programming/Hashes/hashes.rb
+++ Intro to Programming/Hashes/hashes.rb
@@ -40,3 +40,21 @@ puts word_lengths("this is fun") #=> {"this"=>4, "is"=>2, "fun"=>3}
 puts word_lengths("When in doubt, leave it out") #=> {"When"=>4, "in"=>2, "doubt,"=>6, "leave"=>5, "it"=>2, "out"=>3}
 
 
+3.Retrieve Values
+#Write a method retrieve_values that takes in two hashes and a key. 
+#The method should return an array containing the values from the two hashes that correspond with the given key.
+
+
+def retrieve_values(hash1, hash2, key)
+  arrObj = []
+  if key==='name'
+    arrObj<< hash1["name"]
+    arrObj<< hash2["name"]
+  else
+     arrObj<< hash1["color"]
+    arrObj<< hash2["color"]
+  end
+   return [arrObj]
+  end
+
+
