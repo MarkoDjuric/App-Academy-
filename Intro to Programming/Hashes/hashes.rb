ef05c7ab2654a671bcbfe36c64aa9aadcc972e61
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-09-28 23:16:55 +0200

    Update hashes.rb

--- Intro to Programming/Hashes/hashes.rb
+++ Intro to Programming/Hashes/hashes.rb
@@ -58,3 +58,18 @@ def retrieve_values(hash1, hash2, key)
   end
 
 
+4.Cat Builder
+#Write a method cat_builder that takes in a name, color, and age. 
+#The method should return a hash representing a cat with those values.
+
+
+def cat_builder(name_str, color_str, age_num)
+    return { "name"=>name_str, "color"=>color_str, "age"=>age_num }
+end
+
+print cat_builder("Whiskers", "orange", 3) #=> {"name"=>"Whiskers", "color"=>"orange", "age"=>3}
+puts
+
+print cat_builder("Salem", "black", 100) #=> {"name"=>"Salem", "color"=>"black", "age"=>100}
+puts
+
