7d81bb1d59a85e9ecd8ffcae47ca03dcfaa59bb5
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-09-28 23:15:50 +0200

    Update hashes.rb

--- Intro to Programming/Hashes/hashes.rb
+++ Intro to Programming/Hashes/hashes.rb
@@ -14,3 +14,29 @@ puts get_full_name(hash1) # => "Michael Jordan, the GOAT"
 
 hash2 = {"first"=>"Fido", "last"=>"McDog", "title"=> "Loyal"}
 puts get_full_name(hash2) # => "Fido McDog, the Loyal"
+
+
+
+2.Word Lengths
+#Write a method word_lengths that takes in a sentence string and returns a hash where every key is a word of the sentence,
+# and its' corresponding value is the length of that word.
+
+
+def word_lengths(sentence)
+  strArr = sentence.split(" ")
+
+  noviObjekat = Hash.new(0)
+  strArr.each do |elem|
+    
+    noviObjekat[elem]+=elem.length
+    
+  end
+  
+  return noviObjekat
+  
+end
+
+puts word_lengths("this is fun") #=> {"this"=>4, "is"=>2, "fun"=>3}
+puts word_lengths("When in doubt, leave it out") #=> {"When"=>4, "in"=>2, "doubt,"=>6, "leave"=>5, "it"=>2, "out"=>3}
+
+
