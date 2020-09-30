c47ec81eaba49bae17492316f410e70ace238a72
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-09-30 22:47:45 +0200

    Update hashes.rb

--- Intro to Programming/Hashes/hashes.rb
+++ Intro to Programming/Hashes/hashes.rb
@@ -96,3 +96,20 @@ end
 
 puts ae_count("everyone can program") #=> {"a"=>2, "e"=>3}
 puts ae_count("keyboard") #=> {"a"=>1, "e"=>1}
+
+
+6.Element Count
+#Write a method element_count that takes in an array and returns a hash representing the count of each element in the array.
+
+
+def element_count(arr)
+  count = Hash.new(0)
+  
+  arr.each do |elem|
+     count[elem]+=1
+  end
+ return  count
+end
+
+puts element_count(["a", "b", "a", "a", "b"]) #=> {"a"=>3, "b"=>2}
+puts element_count(["red", "red", "blue", "green"]) #=> {"red"=>2, "blue"=>1, "green"=>1}
