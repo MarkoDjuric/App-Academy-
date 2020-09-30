54469de0440ea55a94647272170953ef8624446a
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-09-30 22:49:27 +0200

    Update hashes.rb

--- Intro to Programming/Hashes/hashes.rb
+++ Intro to Programming/Hashes/hashes.rb
@@ -138,6 +138,36 @@ print select_upcase_keys({"DATE"=>"July 4th","holiday"=> "Independence Day", "ty
 puts
 
 
+8..Hand Score
+#Write a method hand_score that takes in a string representing a hand of cards and returns it's total score.
+#You can assume the letters in the string are only A, K, Q, J. A is worth 4 points, K is 3 points, Q is 2 points, and J is 1 point. 
+#The letters of the input string not necessarily uppercase.
+
+
+def hand_score(hand)
+  object = {
+    'A' => 4,
+    'a' => 4,
+    'K' => 3,
+    'k' => 3,
+    'Q' => 2,
+    'q' => 2,
+    'J' => 1,
+    'j' => 1
+  }
+  
+counter=0
+  hand.each_char do  |char| 
+     counter+=object[char] 
+end
+  return counter
+end
+
+puts hand_score("AQAJ") #=> 11
+puts hand_score("jJka") #=> 9
+
+
+
 
 
 
