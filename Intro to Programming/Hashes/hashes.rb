e105bd255f2dd8a1cfadc00edb3bf3a5fa1e6f7e
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-10-02 23:49:34 +0200

    Update hashes.rb

--- Intro to Programming/Hashes/hashes.rb
+++ Intro to Programming/Hashes/hashes.rb
@@ -198,6 +198,22 @@ print frequent_letters('bootcamp') #=> []
 puts
 
 
+10.Unique Elements
+#Write a method unique_elements that takes in an array and returns a new array where all duplicate elements are removed.
+#Solve this using a hash.
+
+
+def unique_elements(arr)
+  hash_elements = {}
+  
+  arr.each { |ele| hash_elements[ele] = true }
+  return hash_elements.keys
+end
+
+print unique_elements(['a', 'b', 'a', 'a', 'b', 'c']) #=> ["a", "b", "c"]
+puts
+
+
 
 
 
