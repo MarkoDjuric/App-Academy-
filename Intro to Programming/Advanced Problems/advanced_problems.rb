58f216435c2a0d6d0596b19f1f02ad45c76b75f7
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-10-05 14:08:33 +0200

    Update advanced_problems.rb

--- Intro to Programming/Advanced Problems/advanced_problems.rb
+++ Intro to Programming/Advanced Problems/advanced_problems.rb
@@ -52,3 +52,22 @@ print map_by_key(locations, "state") #=> ["New York", "California", "Oregon"]
 puts
 print map_by_key(locations, "city") #=> ["New York City", "San Francisco", "Portland"]
 puts
+
+
+50.Yell Sentence
+#Write a method yell_sentence that takes in a sentence string and returns a new sentence where every word is yelled. 
+#See the examples. Use map to solve this.
+
+
+def yell_sentence(sent)
+  
+  
+  array_word = sent.split(" ")
+   
+    sentence =  array_word.map { |word| word.upcase + "!"  }
+  
+    print  sentence.join(" ")
+  
+end
+
+puts yell_sentence("I have a bad feeling about this") #=> "I! HAVE! A! BAD! FEELING! ABOUT! THIS!"
