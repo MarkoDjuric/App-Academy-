094bb7fe5c9ed7b1d221b48f6ba639a536bcfc0a
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-10-06 23:37:32 +0200

    Update advanced_problems.rb

--- Intro to Programming/Advanced Problems/advanced_problems.rb
+++ Intro to Programming/Advanced Problems/advanced_problems.rb
@@ -94,4 +94,26 @@ puts
 
 
 
+53.Last Index
+#Write a method last_index that takes in a string and a character. 
+#The method should return the last index where the character can be found in the string.
+
+
+def last_index(str, char)
+
+ kontra_str =  str.reverse
+  
+   index = kontra_str.index(char)
+  
+  print kontra_str.length-1 - index
+end
+
+puts last_index("octagon", "o")    #=> 5
+puts last_index("programming", "m")#=> 7
+puts last_index("abca", "a")       #=> 3
+puts last_index("mississipi", "i") #=> 9
+
+
+
+
 
