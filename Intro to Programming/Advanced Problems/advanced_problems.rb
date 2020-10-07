4e3becc8423ac9201b4ad785fc10fa2b780cd78e
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-10-07 10:41:45 +0200

    Update advanced_problems.rb

--- Intro to Programming/Advanced Problems/advanced_problems.rb
+++ Intro to Programming/Advanced Problems/advanced_problems.rb
@@ -170,3 +170,36 @@ end
 
 print most_vowels("what a wonderful life") #=> "wonderful"
 
+
+
+
+8.Prime
+#Write a method prime? that takes in a number and returns a boolean, indicating whether the number is prime.
+#A prime number is only divisible by 1 and itself.
+
+def prime?(num)
+  
+   if num < 2
+     return false
+   end
+
+  (2...num).each do |factor|
+     (2...num).each do |f|
+    
+        if factor*f == num
+          return false
+         
+        end
+     end
+   end
+  
+  return true
+ end
+
+
+puts prime?(2)  #=> true
+puts prime?(5)  #=> true
+puts prime?(11) #=> true
+puts prime?(4)  #=> false
+puts prime?(9)  #=> false
+puts prime?(-5) #=> false
