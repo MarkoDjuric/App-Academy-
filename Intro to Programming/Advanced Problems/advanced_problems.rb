2ae24e096f00e42d3c988ff87a1113475e8a5595
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-10-10 10:03:23 +0200

    Update advanced_problems.rb

--- Intro to Programming/Advanced Problems/advanced_problems.rb
+++ Intro to Programming/Advanced Problems/advanced_problems.rb
@@ -334,3 +334,51 @@ end
 
 print summation_sequence(3, 4) # => [3, 6, 21, 231]
 print summation_sequence(5, 3) # => [5, 15, 120]
+
+
+13.Fibonacci
+#The fibonacci sequence is a sequence of numbers whose first and second elements are 1. 
+#To generate further elements of the sequence we take the sum of the previous two elements. 
+#For example the first 6 fibonacci numbers are 1, 1, 2, 3, 5, 8. 
+#Write a method fibonacci that takes in a number length and returns the fibonacci sequence up to the given length.
+
+
+def fibonacci(length)
+  fibonacci_array = []
+  counter = 1
+  
+  if length < 1
+    
+    return []
+    
+  elsif length == 1
+    
+    return  fibonacci_array << 1 
+    
+  elsif length > 1
+    
+     fibonacci_array << 1 << 1
+    
+       (1...length-1).each do |num|
+       
+         last =  fibonacci_array[-1] + fibonacci_array[-2]
+       
+          fibonacci_array << last
+        end
+   end
+      
+    return   fibonacci_array
+  end
+  
+print fibonacci(0) # => []
+puts
+print fibonacci(1) # => [1]
+puts
+print fibonacci(6) # => [1, 1, 2, 3, 5, 8]
+puts
+print fibonacci(8) # => [1, 1, 2, 3, 5, 8, 13, 21]
+puts
+
+
+14.
+
